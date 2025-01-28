83e4bd773048db36c53c706da9e50c6edc894e8e
Amanda <amandavictoria11@outlook.com>
2025-01-28 11:28:12 -0300

    commit

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,23 +4,22 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
 export class HomeComponent {
   telefoneForm: FormGroup;
   erro: boolean = false;
 
   constructor(private fb: FormBuilder) {
-
     this.telefoneForm = this.fb.group({
-      telefone: ['', [Validators.required, Validators.pattern(/^\d{11}$/)]]
+      telefone: ['', [Validators.required, Validators.pattern(/^\d{11}$/)]],
     });
   }
 
   ligar() {
-    if(this.telefoneForm.get('telefone')?.valid){
+    if (this.telefoneForm.get('telefone')?.valid) {
       alert('Ligando para ' + this.telefoneForm.get('telefone')?.value);
     }
-   
+    this.telefoneForm.get('telefone')?.reset();
   }
 }
